161eab81b702c518e2de8bc34fbe7543796c7e5e
xvisox <hubertmirek@gmail.com>
2023-12-30 16:49:02 +0100

    TestContainers setup

--- src/test/java/pl/mimuw/notifier/AbstractIT.java
+++ src/test/java/pl/mimuw/notifier/AbstractIT.java
@@ -5,12 +5,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @DirtiesContext
 @AutoConfigureObservability
 @ActiveProfiles("it-test")
+@Testcontainers
 @SpringBootTest(classes = NotifierServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIT {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @LocalServerPort
     protected Integer port;
